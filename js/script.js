3245adb85b0a18d5bacb1400acb640411c689119
Kevin Bueno <git@github.com:KevzBueno101/Portfolio.git>
2025-07-20 14:24:33 +0800

    added functionality to toggle the sidebar and change the icon on click.
    added headline in home page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const toggleBtn = document.getElementById('toggleBtn');
 const sidebar = document.getElementById('sidebar');
 
@@ -10,3 +11,24 @@ toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
   }
 });
+=======
+const toggleBtn = document.getElementById('toggleBtn');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  if (window.innerWidth <= 768) {
+    // Mobile view: show/hide sidebar
+    sidebar.classList.toggle('active');
+  } else {
+    // Desktop view: collapse/expand sidebar
+    sidebar.classList.toggle('collapsed');
+  }
+});
+
+
+// Toggle icon change on click
+ document.getElementById('toggleIcon').addEventListener('click', function () {
+  this.classList.toggle('fa-angle-left');
+  this.classList.toggle('fa-angle-right');
+});
+>>>>>>> a0a397e (added functionality to toggle the sidebar and)
